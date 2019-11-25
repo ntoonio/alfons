0eef9b575008953cae4ed81fe42876a218a2b823
Anton Lindroth <ntoonio@gmail.com>
2019-11-25 08:27:43 +0100

    Changed orded of actions in install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
+# Install requirements, if not done automatically
+pip3 install -r requirements.txt
+
 # Make Alfons a service
 sudo cp extra/alfons.service /etc/systemd/system/alfons.service
 sudo systemctl daemon-reload
 sudo systemctl enable alfons
 sudo systemctl start alfons
-
-# Install requirements, if not done automatically
-pip3 install -r requirements.txt
